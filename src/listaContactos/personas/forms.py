19dbb3e097b4aba8fcf38bb7aaa800be4a4db005
hernanchoquehuanca <hchoquehuancaz@unsa.edu.pe>
2024-06-17 18:00:01 -0500

    Agregando la clase rawPersonasForm en forms.py pero sin heredar de ModelForm

--- src/listaContactos/personas/forms.py
+++ src/listaContactos/personas/forms.py
@@ -10,3 +10,9 @@ class PersonaForm(forms.ModelForm):
             'edad',
             'donador'
         ]
+
+class rawPersonaForm(forms.Form):
+    nombres = forms.CharField()
+    apellidos = forms.CharField()
+    edad = forms.IntegerField()
+    donador = forms.BooleanField()
