14f01b865e69ba6a485e7a16dfe1e117f0a24467
hernanchoquehuanca <hchoquehuancaz@unsa.edu.pe>
2024-06-17 19:49:04 -0500

    Creando enlaces dinamicos de URL en personasList, para mostrar a las personas y que contengan un enlace hacia su objeto

--- src/listaContactos/personas/models.py
+++ src/listaContactos/personas/models.py
@@ -5,3 +5,7 @@ class Persona(models.Model):
     apellidos = models.TextField(max_length=100)
     edad      = models.IntegerField(blank=True)
     donador   = models.BooleanField()
+
+    def get_absolute_url(self):
+        return "/personas/" + str(self.id) + "/"
+    
