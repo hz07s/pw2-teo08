56b4eccb5d7c1ef2864cbd14829bff9da60d3330
hernanchoquehuanca <hchoquehuancaz@unsa.edu.pe>
2024-06-17 19:29:04 -0500

    Probando redirect para redireccionar al borrar una personas de la base de datos

--- src/listaContactos/personas/views.py
+++ src/listaContactos/personas/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Persona
 from .forms import PersonaForm, rawPersonaForm
 
@@ -61,6 +61,7 @@ def personasDeleteView(request, myID):
     if request.method == 'POST':
         print('LO BORRO')
         obj.delete()
+        return redirect('../')
     context ={
         'objeto' : obj,
     }
